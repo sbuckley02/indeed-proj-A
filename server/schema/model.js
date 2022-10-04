086f3cad4e8bfe179844a5d061769955d1b8bc55
ethantorone <etorone3@gatech.edu>
2022-10-04 00:08:30 -0400

    preliminary fields

--- server/schema/model.js
+++ server/schema/model.js
@@ -3,13 +3,28 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    
+    userId: Number,
+    username: String,
+    password: String,
+    email: String,
+    phoneNumber: String,
+    skills: [String],
 })
 
 const jobListSchema = new Schema({
+    jobId: Number,
+    employer: String,
+    jobTitle: String,
+    description: String,
+    requirements: [String]
 
 })
 
 const employerSchema = new Schema({
+    employerId: Number,
+    username: String,
+    password: String,
+    email: String,
+    
 
 })
