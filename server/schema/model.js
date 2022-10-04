7dd31d41d0996283d7e27756defc83efc88d8421
ethantorone <etorone3@gatech.edu>
2022-10-03 23:55:07 -0400

    created three initial tables

--- server/schema/model.js
+++ server/schema/model.js
@@ -2,3 +2,14 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 
+const userSchema = new Schema({
+    
+})
+
+const jobListSchema = new Schema({
+
+})
+
+const employerSchema = new Schema({
+
+})
