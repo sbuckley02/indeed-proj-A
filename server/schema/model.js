f13207836873de9db46d3e420ccc586878e7a5ea
ethantorone <etorone3@gatech.edu>
2022-10-04 11:54:24 -0400

    schema a bit more fleshed out

--- server/schema/model.js
+++ server/schema/model.js
@@ -9,12 +9,36 @@ const userSchema = new Schema({
     email: String,
     phoneNumber: String,
     skills: [String],
+    educationHistory: [
+        {
+            school: String,
+            degreeType: String,
+            startDate: Date,
+            endDate: Date
+        }
+    ],
+    workHistory: [
+        {
+            employer: String,
+            jobTitle: String,
+            startDate: Date,
+            endDate: Date,
+            description: String
+        }
+    ]
 })
 
 const jobListSchema = new Schema({
     jobId: Number,
     employer: String,
+    datePosted: Date,
     jobTitle: String,
+    payRange: {
+        lowEnd: Number,
+        highEnd: Number
+    },
+    location: [String],
+    fullTime: Boolean,
     description: String,
     requirements: [String]
 
@@ -25,6 +49,6 @@ const employerSchema = new Schema({
     username: String,
     password: String,
     email: String,
-    
+    jobs: [Number]
 
 })
